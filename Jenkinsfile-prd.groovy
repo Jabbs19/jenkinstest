542d5a21ef49865c4012b143c2947b666f7cd082
Mark Jablonski <mcjablonski@gmail.com>
2019-08-24 17:16:54 -0500

    a

--- Jenkinsfile-prd.groovy
+++ Jenkinsfile-prd.groovy
@@ -38,78 +38,9 @@ podTemplate(
                     echo 'Executing on Master'
                 } else if (env.BRANCH_NAME == 'develop'){
                     echo 'Execute on Develop'
-                } else if (env.BRANCH_NAME == 'release'){
-                    echo 'Execute on Release'
-                } else if (env.BRANCH_NAME.contains(TAG_FILTER) && COMMIT_HEAD_TAG.contains(TAG_FILTER)){
-                    echo 'Execute on "prod" specific tag.'
-                }
+                } 
                 else {
                     echo 'Non Branch or Tag execution.'
-                    echo 'Head Tag:' + COMMIT_HEAD_TAG
-                    echo 'Branch Name:' + env.BRANCH_NAME
-                }
-            }
-        }
-        stage('Unit Test') {
-            container('ruby') {
-                if (env.BRANCH_NAME == 'master') {
-                    echo 'No Unit Test'
-                } else if (env.BRANCH_NAME == 'develop'){
-                    echo 'No Unit Test'
-                } else if (env.BRANCH_NAME == 'release'){
-                    echo 'Execute Unit Tests'
-                    sh 'ruby -v'
-                } else if (env.BRANCH_NAME.contains(TAG_FILTER) && COMMIT_HEAD_TAG.contains(TAG_FILTER)){
-                    echo 'No Unit Test'
-                }
-                else {
-                    echo 'No Unit Test'
-                    echo 'Head Tag:' + COMMIT_HEAD_TAG
-                    echo 'Branch Name:' + env.BRANCH_NAME
-                }
-            }
-        }
-        stage('Checkmarx') {
-            container('jnlp') {
-                if (env.BRANCH_NAME == 'master') {
-                    echo 'No Checkmarx'
-                } else if (env.BRANCH_NAME == 'develop'){
-                    echo 'No Checkmarx'
-                } else if (env.BRANCH_NAME == 'release'){
-                    echo 'Execute Checkmarx'
-                } else if (env.BRANCH_NAME.contains(TAG_FILTER) && COMMIT_HEAD_TAG.contains(TAG_FILTER)){
-                    echo 'No Checkmarx'
-                }
-                else {
-                    echo 'No Checkmarx'
-                    echo 'Head Tag:' + COMMIT_HEAD_TAG
-                    echo 'Branch Name:' + env.BRANCH_NAME
-                }
-            }
-        }
-        stage('Secrets') {
-            container('openshift') {
-                echo 'Probably need to build secrets in all environments.'
-            }
-        }
-
-        stage('Build Image') {
-            container('openshift') {
-                if (env.BRANCH_NAME == 'master') {
-                    echo 'No Build'
-                } else if (env.BRANCH_NAME == 'develop'){
-                    echo 'Build on Develop'
-                    echo 'Image Tag:' + DEVELOP_IMAGE_TAG
-                    sh 'oc whoami'
-                } else if (env.BRANCH_NAME == 'release'){
-                    echo 'Build on Release'
-                    echo 'Image Tag:' + RELEASE_IMAGE_TAG
-                    sh 'oc whoami'
-                } else if (env.BRANCH_NAME.contains('prod') && COMMIT_HEAD_TAG.contains('prod')){
-                    echo 'No Build'
-                }
-                else {
-                    echo 'No Build. Should we name feature, or leave open?'
                     echo 'Head Tag:' + COMMIT_HEAD_TAG
                     echo 'Branch Name:' + env.BRANCH_NAME
                 }
@@ -124,16 +55,7 @@ podTemplate(
                     echo 'Develop Deploy'
                     echo 'Image Used:' + DEVELOP_IMAGE_TAG
                     sh 'helm --help'
-                } else if (env.BRANCH_NAME == 'release'){
-                    //Image was just rebuilt again.
-                    echo 'Release Deploy'
-                    echo 'Image Tag:' + RELEASE_IMAGE_TAG
-                    sh 'helm --help'
-                } else if (env.BRANCH_NAME.contains('prod') && COMMIT_HEAD_TAG.contains('prod')){
-                    echo 'Deploy on GitTag'
-                    echo 'Image Tag:' + COMMIT_HEAD_TAG
-                    sh 'helm --help'
-                } else {
+                }  else {
                     echo 'No Deploy. Should we name feature, or leave open?'
                     echo 'Head Tag:' + COMMIT_HEAD_TAG
                     echo 'Branch Name:' + env.BRANCH_NAME
